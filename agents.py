67750effc7217238084dead9038ff03161994b45
David Grogan <dgrogan@chromium.org>
2020-10-24 14:31:08 -0700

    Play 5000 games
    Turn limit (400) player is a little better than heuristic player

--- agents.py
+++ agents.py
@@ -17,6 +17,40 @@ class TurnLimitPlayer:
   def GetAction(self, state):
     scoring_options = state["scoring_options"]
     turn_points = state["turn_points"] + max(scoring_options.values())
+    num_dice_rolled = len(state["just_rolled"])
+    if num_dice_rolled in scoring_options:
+      # We always re-roll 6 dice
+      return num_dice_rolled
     if turn_points > self.turn_limit:
+      return -1
+    if num_dice_rolled - max(scoring_options.keys()) == 1:
+      # We don't roll 1 die
+      return -1
+    return max(scoring_options.keys())
+
+class HeuristicPlayer:
+  def GetAction(self, state):
+    scoring_options = state["scoring_options"]
+    num_dice_rolled = len(state["just_rolled"])
+    scores = state["scores"]
+    if num_dice_rolled in scoring_options:
+      # We always re-roll 6 dice
+      return num_dice_rolled
+
+    way_behind = False
+    points_ahead = scores[0] - max(scores[1:])
+    if points_ahead > 1500:
+      turn_limit = 350
+    elif points_ahead > -2000:
+      turn_limit = 500
+    else:
+      way_behind = True
+      turn_limit = 600
+
+    turn_points = state["turn_points"] + max(scoring_options.values())
+    if turn_points > turn_limit:
+      return -1
+    if num_dice_rolled - max(scoring_options.keys()) == 1:
+      # We don't roll 1 die
       return -1
     return max(scoring_options.keys())
--- main.py
+++ main.py
@@ -2,58 +2,72 @@
 # -*- coding: utf-8 -*-
 
 import random
-import numpy as np
 import sys
 import copy
+
+import numpy as np
+import matplotlib.pyplot as plt
 
 import scoring
 import agents
 
-seed = 12
+seed = 1234
 random.seed(seed)
 np.random.seed(seed)
 
-goal_score = 5000
+goal_score = 10000
 
-players = [agents.TurnLimitPlayer(300), agents.TurnLimitPlayer(500),
-           agents.TurnLimitPlayer(800)]
+players = [agents.TurnLimitPlayer(400),
+           agents.HeuristicPlayer()]
 num_players = len(players)
-scores = [0] * num_players
+winners = [0] * num_players
+NUM_GAMES = 5000
 
-turn = -1
-while max(scores) < goal_score:
-  turn = (turn + 1) % num_players
-  dice_left = 6
-  turn_points = 0
-  print(f"\nplayer {turn} starts rolling, scores are ", scores)
-  while True:
-    roll_result = np.random.randint(1, 7, dice_left)
-    print("roll", sorted(roll_result))
-    scoring_options = scoring.get_score_options(roll_result)
-    if len(scoring_options) == 0:
-      print ("that's a womp")
-      break
-    state = {
-      # Rotate scores so that yours is first
-      "scores": scores[turn:] + scores[:turn],
-      "turn_points": turn_points,
-      "just_rolled": roll_result,
-      "scoring_options": scoring_options,
-      }
-    num_dice_to_score = players[turn].GetAction(state)
-    if num_dice_to_score == -1:
-      # Dude wants to bank and end his turn.
-      turn_points += max(scoring_options.values())
-      scores[turn] += turn_points
-      print (f"He banked his {turn_points} points")
-      break
-    assert num_dice_to_score in scoring_options
-    dice_left -= num_dice_to_score
-    turn_points += scoring_options[num_dice_to_score]
-    if dice_left == 0:
-      print(f"{turn_points} AND ROLLING")
-      dice_left = 6
-    else:
-      print("turn points are now", turn_points)
-      
-print (f"\nplayer {turn} won, with {scores[turn]} points")
+turn_scores_banked = []
+
+for x in range(NUM_GAMES):
+  if x < 2500:
+    turn = -1
+  else:
+    turn = 0
+  scores = [0] * num_players
+  while max(scores) < goal_score:
+    turn = (turn + 1) % num_players
+    dice_left = 6
+    turn_points = 0
+    print(f"\nplayer {turn} starts rolling, scores are ", scores)
+    while True:
+      roll_result = np.random.randint(1, 7, dice_left)
+      print("roll", sorted(roll_result))
+      scoring_options = scoring.get_score_options(roll_result)
+      if len(scoring_options) == 0:
+        print ("that's a womp")
+        turn_scores_banked.append(0)
+        break
+      state = {
+        # Rotate scores so that yours is first
+        "scores": scores[turn:] + scores[:turn],
+        "turn_points": turn_points,
+        "just_rolled": roll_result,
+        "scoring_options": scoring_options,
+        }
+      num_dice_to_score = players[turn].GetAction(state)
+      if num_dice_to_score == -1:
+        # Dude wants to bank and end his turn.
+        turn_points += max(scoring_options.values())
+        scores[turn] += turn_points
+        print (f"He banked his {turn_points} points")
+        turn_scores_banked.append(turn_points)
+        break
+      assert num_dice_to_score in scoring_options
+      dice_left -= num_dice_to_score
+      turn_points += scoring_options[num_dice_to_score]
+      if dice_left == 0:
+        print(f"{turn_points} AND ROLLING")
+        dice_left = 6
+      else:
+        print("turn points are now", turn_points)
+  print (f"\nplayer {turn} won, with {scores[turn]} points")
+  winners[turn] += 1
+
+print ("winner counts are", winners)
